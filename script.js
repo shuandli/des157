5579ca79a707a3a0e6540c54e54fae71a6c9d70c
shuandli <adeli@ucdavis.edu>
2017-01-17 10:00:35 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+//andrew li | design 157 | interactive banner for portal
+
+function setup() {
+	var myCanvas = createCanvas(800, 250);
+	//parent the myCanvas variable to the html element titled “mySketch”
+	myCanvas.parent('mySketch');
+}
+
+function draw() {
+	background(0); //black
+
+	line(mouseX,mouseY,mouseX,mouseY);
+  stroke("#fffff"); //top lines
+  line(100,0,mouseX,mouseY);
+  line(200,0,mouseX,mouseY);
+  line(300,0,mouseX,mouseY);
+  line(400,0,mouseX,mouseY);
+  line(500,0,mouseX,mouseY);
+  line(600,0,mouseX,mouseY);
+  line(700,0,mouseX,mouseY);
+
+	//bottom lines
+  line(100,250,mouseX,mouseY);
+  line(200,250,mouseX,mouseY);
+  line(300,250,mouseX,mouseY);
+  line(400,250,mouseX,mouseY);
+  line(500,250,mouseX,mouseY);
+  line(600,250,mouseX,mouseY);
+  line(700,250,mouseX,mouseY);
+
+  stroke("#66ffff"); //left lines
+  line(0,50,mouseX,mouseY);
+  line(0,100,mouseX,mouseY);
+  line(0,150,mouseX,mouseY);
+  line(0,200,mouseX,mouseY);
+  line(0,250,mouseX,mouseY);
+
+	//right lines
+  line(mouseX,mouseY,800,50);
+  line(mouseX,mouseY,800,100);
+  line(mouseX,mouseY,800,150);
+  line(mouseX,mouseY,800,200);
+  line(mouseX,mouseY,800,250);
+}

ae43a400fdf14fc36716e33b6ee72358b60b7baa
shuandli <adeli@ucdavis.edu>
2017-02-20 14:55:45 -0800

    studio5

--- studio5/script.js
+++ studio5/script.js
@@ -1,3 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
 console.log("reading");
 });
+
+//variables
+var bounce=document.getElementById('bounce');
+var bounceHover=document.getElementById('bounceHover');
+
+bounce.addEventListener('mouseover', function(){
+  console.log('mouseover on bounce');
+  
+
+  function showbounceHover(){
+    console.log('show text');
+    bounceHover.style.opacity=1;
+    bounceHover.style.transition='all .5s';
+  };
+});
+
+bounce.addEventListener('mouseout', function(){
+  console.log('mouseout on bounce');
+  clearTimeout(tipTimer);
+  bounceHover.style.opacity=0;
+});

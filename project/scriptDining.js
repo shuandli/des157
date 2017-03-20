b168722d8118b3954e079bfe6089bfd30248deb2
shuandli <adeli@ucdavis.edu>
2017-03-19 20:23:14 -0700

    rearranging

--- project/scriptDining.js
+++ project/scriptDining.js
@@ -41,23 +41,4 @@ document.addEventListener('DOMContentLoaded', function() {
     foodClose.addEventListener('click', function() {
         foodInfo.style.opacity = 0;
     });
-
-    //chair hover event listener
-    chair.addEventListener('mouseover', function() {
-        chair.style.opacity = 1;
-        chair.src = 'images/theChair.png';
-    });
-    //chair mouseout event listener
-    chair.addEventListener('mouseout', function() {
-        chair.style.opacity = .5;
-        chair.src = 'images/theChair.png';
-    });
-    //chair click
-    chair.addEventListener('click', function() {
-        chairInfo.style.opacity = 1;
-    });
-    //close info
-    chairClose.addEventListener('click', function() {
-        tvInfo.style.opacity = 0;
-    });
 });
